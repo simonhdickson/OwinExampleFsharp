4292abfb5bdf7b5fc2f6cd20707d408b9f13cdeb
Simon <simon.dickson@pebblecode.com>
2013-09-13 11:17:52 +0100

    Intial commit

---
+++ OwinExampleFSharp/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open Owin
+open Microsoft.AspNet.SignalR
+open Microsoft.Owin.StaticFiles
+open Microsoft.Owin.Hosting
+
+open ImpromptuInterface.FSharp
+
+type Move() =
+    inherit Hub()
+    member this.Action(x: int, y: int) : unit =
+        this.Clients.All?Action(x, y)
+
+type Startup() =
+    member x.Configuration(app: Owin.IAppBuilder) =
+        let config = new HubConfiguration()
+
+        app.UseStaticFiles("Web") |> ignore
+        app.MapSignalR(config) |> ignore
+
+[<EntryPoint>]
+let main argv =
+    let url = "http://localhost:5000/"
+    let disposable = WebApp.Start<Startup>(url)
+    Console.WriteLine("Server running on " + url)
+    Console.WriteLine("Press Enter to stop.")
+    Console.ReadLine() |> ignore
+    disposable.Dispose()
+    0
